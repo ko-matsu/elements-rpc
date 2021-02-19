0f0fd3702c90eca4cccbb89e609f9552e280722e
k-matsuzawa <tangrambs803@gmail.com>
2021-02-19 15:18:45 +0900

    update

--- tx_search.sh
+++ tx_search.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-bitcoin_bin=/d/workspace/7_install_wsl/dest/bin/bitcoin-cli
-bitcoin_data_dir=/d/workspace/2_personal-repository/0_bitcoin/local_test/bitcoin
+if [ $# -ne 4 ]; then
+  echo "usage: tx_search.sh startBlockHeight endBlockHeight targetTxid targetVout"
+  exit 1
+fi
 
-targetTxid=195d6c18afaa6c33dcc2da86c6c0cd3f93ec2b44e4c8e9be00c7000eafa1805b
-targetVout=0
-startIdx=417
-endIdx=420
+bitcoin_bin=elements-cli
+# bitcoin_bin=/d/workspace/7_install_wsl/dest/bin/bitcoin-cli
+bitcoin_data_dir=
+# bitcoin_data_dir=-datadir=/d/workspace/2_personal-repository/0_bitcoin/local_test/bitcoin
+
+startIdx=$1
+endIdx=$2
+targetTxid=$3
+targetVout=$4
 
 for offset in $(seq 0 $(($endIdx - $startIdx))); do
   i=$(expr $startIdx + $offset)
   # echo $i
-  hash=$($bitcoin_bin -regtest -datadir=$bitcoin_data_dir getblockhash $i)
-  txs=$($bitcoin_bin -regtest -datadir=$bitcoin_data_dir getblock $hash | jq -c ".tx")
+  hash=$($bitcoin_bin -regtest $bitcoin_data_dir getblockhash $i)
+  txs=$($bitcoin_bin -regtest $bitcoin_data_dir getblock $hash | jq -c ".tx")
   len=$(echo "$txs" | jq length)
   # echo blockhash=$hash
   # echo txids=$txs
@@ -21,14 +28,14 @@ for offset in $(seq 0 $(($endIdx - $startIdx))); do
   for j in $(seq 0 $(($len - 1))); do
     tx=$(echo "$txs" | jq -r .[$j])
     # echo "$j, $tx"
-    vins=$($bitcoin_bin -regtest -datadir=$bitcoin_data_dir getrawtransaction $tx true | jq -c ".vin[] | select(.txid == \"$targetTxid\" and .vout == $targetVout)")
+    vins=$($bitcoin_bin -regtest $bitcoin_data_dir getrawtransaction $tx true | jq -c ".vin[] | select(.txid == \"$targetTxid\" and .vout == $targetVout)")
     # echo $vins
     # vinlen=$(echo $vins | jq length)
     # echo "$vinlen"
 
     if [ $vins ]; then
       echo "txid=$tx"
-      $bitcoin_bin -regtest -datadir=$bitcoin_data_dir getrawtransaction $tx true
+      $bitcoin_bin -regtest $bitcoin_data_dir getrawtransaction $tx true
     fi
   done
 done
